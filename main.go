cebb99f94960f5359f0639f489be7fd2511fcbc0
Dikshant <dikshant.jiwani@gmail.com>
2024-06-03 00:13:55 +0530

    Created a route for listig popular URLs

--- main.go
+++ main.go
@@ -59,8 +59,12 @@ func main() {
 		requests.GetLinkByID(c, db)
 	})
 
-	router.POST("/analytics/:id", func(c *gin.Context) {
+	router.GET("/analytics/:id", func(c *gin.Context) {
 		requests.CountByID(c, db)
+	})
+
+	router.GET("/analytics", func(c *gin.Context) {
+		requests.Popular(c, db)
 	})
 
 	//  ----------------  END : Routes defined here  ----------------
